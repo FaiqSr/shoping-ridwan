1fa59639a7de24d027f3fd4237fb203eb6342baa
FaiqSr <FaiqSr@github.com>
2024-10-24 22:26:11 +0700

    add: menambahkan footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,17 @@
+<footer class="bg-green-100 text-center lg:text-left">
+    <div class="p-10 flex flex-col items-centers justify-center gap-5 sm:gap-0 sm:justify-between sm:flex-row">
+        <h1 class="text-4xl font-bold">Bubro <span class="text-green-400">Farm</span></h1>
+        <div class="flex items-center justify-center gap-5">
+            <a href="https://maps.app.goo.gl/FJ4WjWs64kbhvH818" class="hover:text-cyan-600"><i
+                    data-feather="map-pin"></i></a>
+            <a href="#" class="hover:text-cyan-600"><i data-feather="phone"></i></a>
+        </div>
+    </div>
+    <div class="text-center p-4 bg-green-200">
+        © 2024 Copyright:
+        <a href="https://instagram.com/n3b">N3B</a>
+    </div>
+</footer>
+<script>
+    feather.replace();
+</script>
