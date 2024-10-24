e6ca993f2d99a47167be805431a7ccce1a7b7942
FaiqSr <FaiqSr@github.com>
2024-10-24 22:26:02 +0700

    change: mengubah view market untuk tidak menggunakan x-layout

--- resources/views/markets.blade.php
+++ resources/views/markets.blade.php
@@ -1,8 +1,18 @@
-<x-layout>
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
+    <title>Market BubroFarm</title>
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+</head>
 
+<body>
+    <x-navbar-market />
     <h1 class="text-center text-5xl font-bold">{{ $title }}</h1>
-
     <div>
         <div>
             <h1 class="text-3xl font-bold mb-3 pl-5">Hewan Musang</h1>
@@ -23,6 +33,7 @@
         </div>
     </div>
     <section class="min-h-svh ">asfasf</section>
+    <x-footer />
+</body>
 
-
-</x-layout>
+</html>
