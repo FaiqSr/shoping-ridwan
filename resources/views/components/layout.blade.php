59222acec0fe38ec0cc4fcd31d42bf376217042b
FaiqSr <FaiqSr@github.com>
2024-10-27 00:21:22 +0700

    change: mengganti warna background

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,17 +1,17 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" class="scroll-smooth">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @vite('resources/css/app.css')
-    <title>Shop</title>
+    <title>Bubro Farm</title>
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <script src="https://unpkg.com/feather-icons"></script>
 </head>
 
-<body>
+<body class="bg-slate-50">
     <x-nav-bar />
     {{ $slot }}
     <x-footer />
