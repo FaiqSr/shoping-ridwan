cad0471624c535b6701435dafa9ea312cd837f96
FaiqSr <FaiqSr@github.com>
2024-10-29 15:06:16 +0700

    add: menambahkan font baru bernama Oswald

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ export default {
         "./resources/**/*.vue",
     ],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                "Oswald": ["Oswald", "sans-serif"]
+            }
+        },
     },
     plugins: [],
 };
