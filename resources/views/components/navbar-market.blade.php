e6aab1328089afd8f790c5df5d89ca4016e07038
FaiqSr <FaiqSr@github.com>
2024-10-29 15:07:07 +0700

    create: membuat navbar untuk market

--- resources/views/components/navbar-market.blade.php
+++ resources/views/components/navbar-market.blade.php
@@ -1,3 +1,11 @@
 <nav>
-
+    <div class="flex justify-between items-center h-20 px-5 shadow-xl">
+        <div><a href="/" class="font-bold text-2xl ">Bubro <span class="text-green-400">Market</span></a>
+        </div>
+        <div class="gap-5 items-center hidden sm:flex">
+            <a href="/" class="hover:text-cyan-600">Home</a>
+        </div>
+        <div>
+            <a href="/cart"><i data-feather="shopping-cart"></i></a>
+        </div>
 </nav>
