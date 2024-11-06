6a0d156149393b2db6130099b985cd70a01c1d37
FaiqSr <FaiqSr@github.com>
2024-11-06 15:55:56 +0700

    add: profile page di navbar market

--- resources/views/components/navbar-market.blade.php
+++ resources/views/components/navbar-market.blade.php
@@ -1,9 +1,14 @@
 <nav>
-    <div class="flex justify-between items-center h-20 px-5 shadow-xl">
+    <div class="flex justify-center items-center h-20 px-5 shadow-xl gap-10">
         <div><a href="/" class="font-bold text-2xl ">Bubro <span class="text-green-400">Market</span></a>
         </div>
         <div class="gap-5 items-center hidden sm:flex">
-            <a href="/" class="hover:text-cyan-600">Home</a>
+            <form action="{{ env('APP_URL') }}/search" class="flex">
+                @csrf
+                <input type="text" name="search" class="w-96 rounded-sm border-green-400" placeholder="Cari...">
+                <div class="flex items-center bg-green-400 px-3"><button type="submit"><i data-feather="search"
+                            class=""></i></button></div>
+            </form>
         </div>
         <div>
             <a href="/cart"><i data-feather="shopping-cart"></i></a>
