408e697d0313bd491a214f9b5366006ca31c103d
FaiqSr <FaiqSr@github.com>
2024-11-06 17:12:05 +0700

    add: menambahkan tentang page

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,8 +1,64 @@
 <x-layout>
-
-    <div class="min-h-svh flex items-center justify-center flex-col gap-5">
-        <h1>{{ $data->name }}</h1>
-        <h3>{{ $data->email }}</h3>
-    </div>
-
+    <section class="flex flex-col sm:flex-row min-h-svh container mx-auto">
+        <article class="px-8 mb-5 mt-10 sm:w-1/4 ">
+            <img src="https://picsum.photos/id/1/1920/1080" alt="" class="w-80 rounded-lg">
+            <h1 class="text-3xl font-bold mt-5">Bubro <span class="text-green-400">Farm</span></h1>
+        </article>
+        <article class="px-8 sm:mt-10 flex flex-col gap-5 sm:w-2/4">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa nobis recusandae reiciendis excepturi,
+                expedita eaque tempore cupiditate exercitationem natus labore at voluptate, modi nesciunt facilis quas
+                error dolore doloremque iure voluptatibus nostrum sint vitae temporibus rerum facere. Dignissimos nihil
+                inventore voluptatem laudantium! Natus minus ad quidem cupiditate, id quos architecto cum, eveniet,
+                illum esse magnam necessitatibus dolores nostrum excepturi animi quis rem. Neque corporis, eos deleniti
+                dolores distinctio iusto consequuntur nulla, maiores voluptatem praesentium nihil reiciendis ea quod
+                nisi eligendi optio hic? Explicabo vero ad nesciunt quisquam facilis assumenda magni quod! Iusto
+                pariatur quo facere illo iure, non itaque temporibus.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam optio explicabo dolorum atque, totam fuga
+                qui quasi dicta fugit recusandae. Deleniti, corporis nemo. Veritatis dolorem, laboriosam eligendi quae
+                incidunt provident!</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni, odio consequatur consectetur possimus
+                cumque minus corrupti laudantium sed obcaecati vero quibusdam. Reprehenderit saepe corporis officiis
+                minima at beatae consequuntur optio!
+            </p>
+        </article>
+        <section class="px-8 mt-5 sm:w-1/4 sm:border-l-2">
+            <h2 class="text-xl font-bold mb-2">News</h2>
+            <article class="flex gap-4 ">
+                <img src="https://picsum.photos/id/1/1920/1080" alt="" class="w-10 h-10">
+                <section>
+                    <h5 class="font-semibold text-sm">Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                        Deleniti, ut.
+                    </h5>
+                    <p class="text-slate-500">lorem</p>
+                </section>
+            </article>
+            <article class="flex gap-4 items-center">
+                <img src="https://picsum.photos/id/2/1920/1080" alt="" class="w-10 h-10">
+                <section>
+                    <h5 class="font-semibold text-sm">Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                        Deleniti, ut.
+                    </h5>
+                    <p class="text-slate-500">lorem</p>
+                </section>
+            </article>
+            <article class="flex gap-4 items-center">
+                <img src="https://picsum.photos/id/3/1920/1080" alt="" class="w-10 h-10">
+                <section>
+                    <h5 class="font-semibold text-sm">Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                        Deleniti, ut.
+                    </h5>
+                    <p class="text-slate-500">lorem</p>
+                </section>
+            </article>
+            <article class="flex gap-4 items-center">
+                <img src="https://picsum.photos/id/4/1920/1080" alt="" class="w-10 h-10">
+                <section>
+                    <h5 class="font-semibold text-sm">Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                        Deleniti, ut.
+                    </h5>
+                    <p class="text-slate-500">lorem</p>
+                </section>
+            </article>
+        </section>
+    </section>
 </x-layout>
