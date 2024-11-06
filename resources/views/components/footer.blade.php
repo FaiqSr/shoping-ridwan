94f65ce8196fde7aa9621dc26399eba695856e78
FaiqSr <FaiqSr@github.com>
2024-11-06 21:09:10 +0700

    change: mengubah dari copyright N3B ke copyright bubrofarm

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="bg-green-100 text-center">
     <div class="text-center p-4 bg-green-200">
-        © 2024 Copyright:
-        <a href="https://instagram.com/n3b">N3B</a>
+        © 2024 Copyright
+        <a href="https://instagram.com/n3b">bubrofarm</a>
     </div>
 </footer>
 <script>
